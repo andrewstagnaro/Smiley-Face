29edb88c07560f7588f172b8968db7666fd3ecab
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-09-12 13:02:48 -0400

    Add files via upload

---
+++ src/Smile.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author drew stagnaro
+ *Smiley Face
+ */
+
+
+public class Smile 
+{
+
+	public static void main(String[] args) 
+	{
+	System.out.print("///////\n");	
+	System.out.print("| * * |\n");
+	System.out.print("|  *  |\n");
+	System.out.print("| \\_/ |\n");
+	System.out.print("\\____/\n");
+	}
+
+}
